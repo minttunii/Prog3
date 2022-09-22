b7effd92f13844d7aa7160030b60f1ed6c55257f
Minttu Niiranen <minttu.m.niiranen@gmail.com>
2022-09-22 20:39:03 +0300

    Valmis Student class

---
+++ Round4/studentregister/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    private final String name;
+    private final int student_number;
+
+    public Student(String name, int student_number) {
+        this.name = name;
+        this.student_number = student_number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStudent_number() {
+        return student_number;
+    }
+    
+
+}
