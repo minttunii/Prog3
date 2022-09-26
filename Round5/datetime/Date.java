3ca74cbe896ccd4812f0f1d5f4ba424e7482a8a3
Minttu Niiranen <minttu.m.niiranen@gmail.com>
2022-09-26 14:12:49 +0300

    Päivämäärä ja aika tehtävä eka versio

---
+++ Round5/datetime/Date.java
@@ -0,0 +1,54 @@
+
+
+public class Date {
+    private final int year;
+    private final int month;
+    private final int day;
+    
+    static boolean isLeapYear(int year) {
+    // Karkausvuosi: jaollinen 4:llä ja ei jaollinen 100:lla tai jaollinen 400:lla.
+    // Javan loogisaritmeettiset operaatiot ja return-lause kuin C-kielessä.
+    return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+    }
+
+    static int[][] mDays = {{31, 31}, {28, 29}, {31, 31}, {30, 30}, {31, 31}, {30, 30},
+                          {31, 31}, {31, 31}, {30, 30}, {31, 31}, {30, 30}, {31, 31}};
+    
+    static int monthDays(int month, int year) {
+    int days = -1;
+    if(1 <= month && month <= 12) {
+      // Ehdollinen operaattori kuin C-kielessä.
+      days = isLeapYear(year) ? mDays[month-1][1] : mDays[month-1][0];
+    }
+    return days;
+  }
+
+    public Date(int year, int month, int day) throws DateException {
+        if((1 <= day) && (day <= monthDays(month, year))){
+            throw new DateException(String.format("Illegal date %2d.%2d.%d%n", 
+                    day, month, year));
+        }
+        
+        this.year = year;
+        this.month = month;
+        this.day = day; 
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+    
+    public String toString(){
+        String date = String.format("%2s"+ "." + "%2s" + "." + "s",
+                Integer.toString(day), Integer.toString(month), Integer.toString(year));
+        return date;
+    }
+}
