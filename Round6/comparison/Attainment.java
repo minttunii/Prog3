1729c99470ed4cad0a764a269886640f70ebc6b9
Minttu Niiranen <minttu.m.niiranen@gmail.com>
2022-10-03 15:49:08 +0300

    Attainment ensimmäinen toteutus

---
+++ Round6/comparison/Attainment.java
@@ -0,0 +1,54 @@
+
+import java.util.Comparator;
+
+
+public class Attainment implements Comparable<Attainment>, Comparator<Attainment>{
+    private final String courseCode;
+    private final String studentNumber;
+    private final int grade;
+    public static final Comparator<Attainment> CODE_STUDENT_CMP = Comparator.comparing(Attainment::getCourseCode).thenComparing(Attainment::getStudentNumber);
+    public static final Comparator<Attainment> CODE_GRADE_CMP = Comparator.comparing(Attainment::getCourseCode).thenComparing(Attainment::getGrade);
+
+    public Attainment(String courseCode, String studentNumber, int grade) {
+        this.courseCode = courseCode;
+        this.studentNumber = studentNumber;
+        this.grade = grade;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("%s %s %d%n", courseCode, studentNumber, grade);
+    }
+    
+    @Override
+    public int compareTo(Attainment other){
+        int cmp = studentNumber.compareTo(other.studentNumber);
+        if(cmp == 0){
+            cmp = courseCode.compareTo(other.courseCode);
+        }
+        return cmp;
+    }  
+    
+    @Override
+    public int compare(Attainment a, Attainment b){
+        int cmp = CODE_STUDENT_CMP.compare(a, b);
+        if(cmp == 0){
+            cmp = CODE_GRADE_CMP.compare(a, b);
+        }
+        return cmp;
+    }
+    
+}
+   
