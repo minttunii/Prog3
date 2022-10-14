8b8e549f32fced015a66a2b7fda5e588d7cac55b
Minttu Niiranen <minttu.m.niiranen@gmail.com>
2022-10-14 16:26:38 +0300

    Json tiedostoon kirjoittaminen

--- Round7/jsoncountries/src/main/java/fi/tuni/prog3/round7/jsoncountries/CountryData.java
+++ Round7/jsoncountries/src/main/java/fi/tuni/prog3/round7/jsoncountries/CountryData.java
@@ -1,12 +1,23 @@
 
 package fi.tuni.prog3.round7.jsoncountries;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class CountryData {
@@ -21,105 +32,139 @@ public class CountryData {
     }
     
     private static void readAreaFile(String areaFile, List<Country> list){
-        
-    JsonObject jsonObject = new JsonParser().parse(areaFile).getAsJsonObject();
-    JsonArray records = jsonObject.get("records").getAsJsonArray();
-    for(JsonElement record : records){
-        JsonArray fields = record.getAsJsonObject().get("field").getAsJsonArray();
-        for(JsonElement field : fields){
-            JsonObject fieldobject = field.getAsJsonObject();
-            String name = fieldobject.get("attributes").getAsJsonObject().get("name").getAsString();
-            String countryname = " ";
-            if("Country or Area".equals(name)){
-                countryname = fieldobject.get("value").getAsString();
-                list.add(new Country(countryname));
-            }
-            if("Value".equals(name)){
-                for(Country c : list){
-                    if(countryname.equals(c.getName())){
-                        double area = fieldobject.get("value").getAsDouble();
-                        c.setArea(area);
+    
+        try {
+            File input = new File(areaFile);
+            JsonElement fileElement = JsonParser.parseReader(new FileReader(input));
+            JsonObject fileObject = fileElement.getAsJsonObject();
+            
+            JsonArray records = fileObject.get("records").getAsJsonArray();
+            for(JsonElement record : records){
+                JsonArray fields = record.getAsJsonObject().get("field").getAsJsonArray();
+                for(JsonElement field : fields){
+                    JsonObject fieldobject = field.getAsJsonObject();
+                    String name = fieldobject.get("attributes").getAsJsonObject().get("name").getAsString();
+                    String countryname = " ";
+                    if("Country or Area".equals(name)){
+                        countryname = fieldobject.get("value").getAsString();
+                        list.add(new Country(countryname));
+                    }
+                    if("Value".equals(name)){
+                        for(Country c : list){
+                            if(countryname.equals(c.getName())){
+                                double area = fieldobject.get("value").getAsDouble();
+                                c.setArea(area);
+                            }
+                        }
                     }
                 }
-            }
+            }   } catch (FileNotFoundException ex) {
+            Logger.getLogger(CountryData.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
     
 } 
 
     
     private static void readPopulationFile(String populationFile, List<Country> list){
         
-    JsonObject jsonObject = new JsonParser().parse(populationFile).getAsJsonObject();
-    JsonArray records = jsonObject.get("records").getAsJsonArray();
-    for(JsonElement record : records){
-        JsonArray fields = record.getAsJsonObject().get("field").getAsJsonArray();
-        for(JsonElement field : fields){
-            JsonObject fieldobject = field.getAsJsonObject();
-            String name = fieldobject.get("attributes").getAsJsonObject().get("name").getAsString();
-            String countryname = " ";
-            if("Country or Area".equals(name)){
-                countryname = fieldobject.get("value").getAsString();
-                boolean exists = false;    
-                    for(Country c : list){
-                        if(countryname.equals(c.getName())){
-                            exists = true;
-                            break;
+        try {
+            File input = new File(populationFile);
+            JsonElement fileElement = JsonParser.parseReader(new FileReader(input));
+            JsonObject fileObject = fileElement.getAsJsonObject();
+            
+            JsonArray records = fileObject.get("records").getAsJsonArray();
+            for(JsonElement record : records){
+                JsonArray fields = record.getAsJsonObject().get("field").getAsJsonArray();
+                for(JsonElement field : fields){
+                    JsonObject fieldobject = field.getAsJsonObject();
+                    String name = fieldobject.get("attributes").getAsJsonObject().get("name").getAsString();
+                    String countryname = " ";
+                    if("Country or Area".equals(name)){
+                        countryname = fieldobject.get("value").getAsString();
+                        boolean exists = false;
+                        for(Country c : list){
+                            if(countryname.equals(c.getName())){
+                                exists = true;
+                                break;
+                            }
+                        }
+                        if(!exists){
+                            list.add(new Country(countryname));
+                        }
+                        
+                    }
+                    if("Value".equals(name)){
+                        for(Country c : list){
+                            if(countryname.equals(c.getName())){
+                                long population = fieldobject.get("value").getAsLong();
+                                c.setPopulation(population);
+                            }
                         }
                     }
-                    if(!exists){
-                        list.add(new Country(countryname));
-                        }            
-                
-            }
-            if("Value".equals(name)){
-                for(Country c : list){
-                    if(countryname.equals(c.getName())){
-                        long population = fieldobject.get("value").getAsLong();
-                        c.setPopulation(population);
-                    }
                 }
-            }
+            }   } catch (FileNotFoundException ex) {
+            Logger.getLogger(CountryData.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
     }
     
     private static void readGdpFile(String gdpFile, List<Country> list){
-    JsonObject jsonObject = new JsonParser().parse(gdpFile).getAsJsonObject();
-    JsonArray records = jsonObject.get("records").getAsJsonArray();
-    for(JsonElement record : records){
-        JsonArray fields = record.getAsJsonObject().get("field").getAsJsonArray();
-        for(JsonElement field : fields){
-            JsonObject fieldobject = field.getAsJsonObject();
-            String name = fieldobject.get("attributes").getAsJsonObject().get("name").getAsString();
-            String countryname = " ";
-            if("Country or Area".equals(name)){
-                countryname = fieldobject.get("value").getAsString();
-                boolean exists = false;    
-                    for(Country c : list){
-                        if(countryname.equals(c.getName())){
-                            exists = true;
-                            break;
+        try {
+            File input = new File(gdpFile);
+            JsonElement fileElement = JsonParser.parseReader(new FileReader(input));
+            JsonObject fileObject = fileElement.getAsJsonObject();
+            JsonArray records = fileObject.get("records").getAsJsonArray();
+            
+            for(JsonElement record : records){
+                JsonArray fields = record.getAsJsonObject().get("field").getAsJsonArray();
+                for(JsonElement field : fields){
+                    JsonObject fieldobject = field.getAsJsonObject();
+                    String name = fieldobject.get("attributes").getAsJsonObject().get("name").getAsString();
+                    String countryname = " ";
+                    if("Country or Area".equals(name)){
+                        countryname = fieldobject.get("value").getAsString();
+                        boolean exists = false;
+                        for(Country c : list){
+                            if(countryname.equals(c.getName())){
+                                exists = true;
+                                break;
+                            }
+                        }
+                        if(!exists){
+                            list.add(new Country(countryname));
+                        }
+                        
+                    }
+                    if("Value".equals(name)){
+                        for(Country c : list){
+                            if(countryname.equals(c.getName())){
+                                double gdp = fieldobject.get("value").getAsDouble();
+                                c.setGDP(gdp);
+                            }
                         }
                     }
-                    if(!exists){
-                        list.add(new Country(countryname));
-                        }            
-                
-            }
-            if("Value".equals(name)){
-                for(Country c : list){
-                    if(countryname.equals(c.getName())){
-                        double gdp = fieldobject.get("value").getAsDouble();
-                        c.setGDP(gdp);
-                    }
                 }
-            }
+            }   } catch (FileNotFoundException ex) {
+            Logger.getLogger(CountryData.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
     }
     
     public static void writeToJson(List<Country> countries, String countryFile){
-        
+        Writer writer = null;
+        try {
+            // Create Gson instance
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            // Create a writer
+            writer = Files.newBufferedWriter(Paths.get(countryFile));
+            gson.toJson(countries, writer);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(CountryData.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException ex) {
+                Logger.getLogger(CountryData.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
